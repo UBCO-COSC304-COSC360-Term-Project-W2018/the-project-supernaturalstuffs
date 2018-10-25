beeab963aafd2bb020d6ba8ab4403ad31a78c046
Robert Rosa <robby.rosa@hotmail.com>
2018-10-24 19:47:38 -0700

    checkout javascript finished

--- src/client/script/checkout-payment.js
+++ src/client/script/checkout-payment.js
@@ -9,19 +9,46 @@ function checkPayment(){
   cardName.addEventListener("change",function(){ cardName.classList.remove('invalid') });
 
   //card number validation
-  var cardName = document.forms["payForm"]["cardNumber"];
-  if(cardName.value == ""){
-    cardName.className = 'invalid';
+  var cardType = document.getElementById("payMethod").value;
+  var cNumber = document.forms["payForm"]["cardNumber"];
+  var firstNum = cNumber.value.charAt(0);
+
+  var hasNonNum = false;
+  for(var i = 0; i < cNumber.value.length; i++){
+    var x = cNumber.value.charAt(0);
+    if(isNaN(x)){
+      hasNonNum = true;
+      break;
+    }
+  }
+
+  if(cNumber.value == ""){
+    cNumber.className = 'invalid';
+    return false;
+  }else if( firstNum == 4 && cardType != "Visa"){
+    alert("Card number and type do not match")
+    return false;
+  }else if( firstNum == 5 && cardType != "Mastercard"){
+    alert("Card number and type do not match")
+    return false;
+  }else if( firstNum == 3 && cardType != "American Express"){
+    alert("Card number and type do not match")
+    return false;
+  }else if(hasNonNum){
+    alert("Card Number can only contain numbers")
     return false;
   }
-  cardName.addEventListener("change",function(){ cardName.classList.remove('invalid') });
+  cNumber.addEventListener("change",function(){ cNumber.classList.remove('invalid') });
 
   //security code validation
-  var cardName = document.forms["payForm"]["secCode"];
-  if(cardName.value == ""){
-    cardName.className = 'invalid';
+  var secCode = document.forms["payForm"]["secCode"];
+  if(secCode.value == ""){
+    secCode.className = 'invalid';
+    return false;
+  }else if(secCode.value.length != 3){
+    alert("Security code must have a length of 3");
     return false;
   }
-  cardName.addEventListener("change",function(){ cardName.classList.remove('invalid') });
-
+  secCode.addEventListener("change",function(){ secCode.classList.remove('invalid') });
+  //make sure they choose a date
 }
