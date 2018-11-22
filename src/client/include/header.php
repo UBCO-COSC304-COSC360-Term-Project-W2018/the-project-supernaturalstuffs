ae8a3ff0f5249c1a2042b038a41ce3ca16129b0e
unknown <courtneygosselin1996@gmail.com>
2018-11-22 15:18:14 -0800

    add include folder
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

---
+++ src/client/include/header.php
@@ -0,0 +1,19 @@
+	<header>
+			<h1 id="title" ><a href="index.html"><img src="./src/client/images/logo.png">Super(natural) Store</a></h1>
+			<div id="search-cart">
+				<input type="text" class="searchHome" placeholder="Search...">
+				<img src="./src/client/images/search.png" alt="search" id="search">
+				<a href="./src/client/html/cart.html"><img src="./src/client/images/cart.png" alt="shopping cart" id="cart"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="./src/client/html/contact-FAQ.php">Contact</a></li>
+					<li><a href="./src/client/html/accountDetails.php">Account</a></li>
+				</ul>
+				<ul id="login-signup">
+					<li><a href="./src/client/html/login.html" class="login-signup">Login</a></li>
+					<li><a href="./src/client/html/createAccount.html" class="login-signup">Signup</a></li>
+				</ul>
+			</nav>
+		</header>
