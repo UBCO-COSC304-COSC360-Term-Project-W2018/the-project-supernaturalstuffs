3a9999c77a02ee92078ddb7178c8e6426bfc4aa5
rosarobert <43102168+rosarobert@users.noreply.github.com>
2018-11-21 16:57:02 -0800

    Update db_credentials.php

--- src/server/include/db_credentials.php
+++ src/server/include/db_credentials.php
@@ -1,7 +1,7 @@
 <?php
-	$username = "rrosa";
-	$password = "54255161";
+	$username = "60186160";
+	$password = "041396";
 	$database = "db_" . $username;
-	$server = "sql04.ok.ubc.ca";
+	$server = $username . "@cosc360.ok.ubc.ca";
 	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
 ?>
