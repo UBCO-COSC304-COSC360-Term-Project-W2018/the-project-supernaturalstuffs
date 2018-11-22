84f6f1b99a4f162c1a21c7a1d64b66e6922dde86
Robert Rosa <robby.rosa@hotmail.com>
2018-11-22 14:55:36 -0800

    create account

---
+++ src/server/createAccount.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <body>
+    <?php
+      include 'include/db_credentials.php';
+
+      /** Get first name **/
+      $custFN = null;
+      if (isset($_POST['fName'])) {
+          $custFN = $_POST['fName'];
+      }
+      /** Get last name **/
+      $custLN = null;
+      if (isset($_POST['lName'])) {
+          $custLN = $_POST['lName'];
+      }
+      /** Get email **/
+      $custE = null;
+      if (isset($_POST['email'])) {
+          $custE = $_POST['email'];
+      }
+      /** Get password **/
+      $custPW = null;
+      if (isset($_POST['password'])) {
+          $custPW = $_POST['password'];
+      }
+
+      //check to see if all not null values are entered
+      if ($custFN == null){
+        $message = "Please enter a first name";
+        echo "<script type='text/javascript'>alert('$message');
+        window.location.href='/src/client/html/createAccount.html'</script>";
+        die();
+      }else if ($custLN == null){
+        $message = "Please enter a last name";
+        echo "<script type='text/javascript'>alert('$message');
+        window.location.href='/src/client/html/createAccount.html'</script>";
+        die();
+      }else if ($custE == null){
+        $message = "Please enter an email";
+        echo "<script type='text/javascript'>alert('$message');
+        window.location.href='/src/client/html/createAccount.html'</script>";
+        die();
+      }else if ($custPW == null){
+        $message = "Please enter a password";
+        echo "<script type='text/javascript'>alert('$message');
+        window.location.href='/src/client/html/createAccount.html'</script>";
+        die();
+      }
+
+      //connect to database
+      try {
+          $pdo = new PDO($dsn, $user, $pass, $options);
+      } catch (\PDOException $e) {
+          throw new \PDOException($e->getMessage(), (int)$e->getCode());
+      }
+
+      $sql = "INSERT INTO User VALUES (DEFAULT ,:username ,:password ,:firstname ,:lastname ,:email)";
+      $custUN = "TEST";
+      $statement = $pdo->prepare($sql);
+      $statement->bindValue(':username', $custUN, PDO::PARAM_STR);
+      $statement->bindValue(':password', $custP, PDO::PARAM_STR);
+      $statement->bindValue(':firstname', $custFN, PDO::PARAM_STR);
+      $statement->bindValue(':lastname', $custLN, PDO::PARAM_STR);
+      $statement->bindValue(':email', $custE, PDO::PARAM_STR);
+      $insert = $statement->execute();
+
+      //change header-pass the user is logged in vie session
+      echo "<script type='text/javascript'>alert('Customer ' . $custFN . ' ' . $custLN . ' was added');
+      window.location.href='/src/client/html/login.html'</script>";
+    ?>
+  </body>
+</html>
