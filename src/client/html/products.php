b5bea77315f98d7df075bf7b972f0bc981cef7d8
unknown <courtneygosselin1996@gmail.com>
2018-11-22 17:03:50 -0800

    PHP files moved
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

---
+++ src/client/html/products.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title></title>
+	  <link href='https://fonts.googleapis.com/css?family=Almendra Display' rel='stylesheet'>
+      <link rel="stylesheet" href="../css/reset.css" />
+      <link rel="stylesheet" href="../css/header-footer.css" />
+      <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+      <link rel="stylesheet" type="text/css" href="../css/Product.css">
+      <!--<script src="script.js"></script>-->
+   </head>
+   <body>
+		<!--Include header-->
+		<?php include '../include/header.php'; ?>
+		<main>
+			<div id="product-sidebar">
+				<h3>Products</h3>
+				<div id="productSidebarOptions">
+				   <h4><a href="products.php">All</a></h4>
+				   <h4>Creatures</h4>
+				   <p>Vampires</p>
+				   <p>Shifters</p>
+				   <p>Ghosts</p>
+				   <p>Werewolves</p>
+				   <h4>Gear</h4>
+				   <p>Silver Bullets</p>
+				   <p>Holy Water</p>
+				</div>
+			 </div>
+			 <div id="contentRight">
+				<p id="sortButton">Sort by</p>
+				<div id="box">
+				   <div class="productBox">
+					  <a href="individualProducts.php"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+				   <div class="productBox">
+					  <a href="individualProducts.html"><img src="../images/ghostbusters-logo.png" alt="productimage"></a>
+					  <p>Product Name</p>
+					  <p>Product Description</p>
+					  <p>Price</p>
+					  <p class="addCart">Add to Cart</p>
+				   </div>
+
+				</div>
+			</div>
+		</main>
+		<!--Include Footer-->
+		<?php include '../include/footer.php' ?>
+   </body>
+   <foot>
+   </foot>
+</html>
