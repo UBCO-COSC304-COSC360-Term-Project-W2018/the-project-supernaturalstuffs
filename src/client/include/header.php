25ef4dd0485c4921dcbd0121b486747da4663695
Courtney Gosselin <courtneygosselin1996@gmail.com>
2018-11-23 00:59:10 +0000

    File path fixes
    Signed-off-by: Courtney Gosselin <courtneygosselin1996@gmail.com>

--- src/client/include/header.php
+++ src/client/include/header.php
@@ -3,17 +3,17 @@
 			<div id="search-cart">
 				<input type="text" class="searchHome" placeholder="Search...">
 				<img src="/src/client/images/search.png" alt="search" id="search">
-				<a href="/src/client/html/cart.html"><img src="/src/client/images/cart.png" alt="shopping cart" id="cart"></a>
+				<a href="/src/client/html/cart.php"><img src="/src/client/images/cart.png" alt="shopping cart" id="cart"></a>
 			</div>
 			<nav>
 				<ul>
-					<li><a href="index.html">Home</a></li>
+					<li><a href="/index.php">Home</a></li>
 					<li><a href="/src/client/html/contact-FAQ.php">Contact</a></li>
 					<li><a href="/src/client/html/accountDetails.php">Account</a></li>
 				</ul>
 				<ul id="login-signup">
-					<li><a href="/src/client/html/login.html" class="login-signup">Login</a></li>
-					<li><a href="/src/client/html/createAccount.html" class="login-signup">Signup</a></li>
+					<li><a href="/src/client/html/login.php" class="login-signup">Login</a></li>
+					<li><a href="/src/client/html/createAccount.php" class="login-signup">Signup</a></li>
 				</ul>
 			</nav>
 		</header>
