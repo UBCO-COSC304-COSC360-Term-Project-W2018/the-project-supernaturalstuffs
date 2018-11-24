f5b9fd3bb05a82112e38bec24dacf035b2041913
unknown <courtneygosselin1996@gmail.com>
2018-11-23 17:06:58 -0800

    Adding Products
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

--- index.php
+++ index.php
@@ -31,9 +31,25 @@
 					<p>Shifters</p>
 					<p>Ghosts</p>
 					<p>Werewolves</p>
+					<p>Angels</p>
+					<p>Deities</p>
 					<h4>Gear</h4>
 					<p>Silver Bullets</p>
 					<p>Holy Water</p>
+					<p>Salt Bullets</p>
+					<p>Wooden Stake</p>
+					<p>Hot Garlic</p>
+					<p>Blessed Rossary</p>
+					<p>Angel Blade</p>
+					<p>Hunter Journal</p>
+					<p>Book of the Damned</p>
+					<p>The Colt</p>
+					<p>The First Blade</p>
+					<p>Death Scythe</p>
+					<p>Demon-Killing Knife</p>
+					<p>Lambs Blood</p>
+					<p>Horn of Gabriel</p>
+					<p>Hand of God</p>
 				</div>
 			</div>
 		</main>
