8be19fddcb7bd2835514c1e94a8d47abc942b312
unknown <courtneygosselin1996@gmail.com>
2018-11-23 17:21:01 -0800

    Product Sidebar file added for includes
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

---
+++ src/server/include/productSidebar.php
@@ -0,0 +1,30 @@
+<div id="product-sidebar">
+	<h3>Products</h3>
+	<div id="productSidebarOptions">
+		<h4><a href="./src/server/PHP/products.php">All</a></h4>
+		<h4>Creatures</h4>
+		<p>Vampires</p>
+		<p>Shifters</p>
+		<p>Ghosts</p>
+		<p>Werewolves</p>
+		<p>Angels</p>
+		<p>Deities</p>
+		<h4>Gear</h4>
+		<p>Silver Bullets</p>
+		<p>Holy Water</p>
+		<p>Salt Bullets</p>
+		<p>Wooden Stake</p>
+		<p>Hot Garlic</p>
+		<p>Blessed Rossary</p>
+		<p>Angel Blade</p>
+		<p>Hunter Journal</p>
+		<p>Book of the Damned</p>
+		<p>The Colt</p>
+		<p>The First Blade</p>
+		<p>Death Scythe</p>
+		<p>Demon-Killing Knife</p>
+		<p>Lambs Blood</p>
+		<p>Horn of Gabriel</p>
+		<p>Hand of God</p>
+	</div>
+</div>
