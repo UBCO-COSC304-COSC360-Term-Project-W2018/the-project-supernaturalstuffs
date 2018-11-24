431ffab92991aad18c94004a6a22ee0443b3a9d3
Robert Rosa <robby.rosa@hotmail.com>
2018-11-24 12:31:39 -0800

    extra feature

--- src/server/PHP/checkoutOldPayment.php
+++ src/server/PHP/checkoutOldPayment.php
@@ -7,7 +7,16 @@
   <body>
     <?php
     session_start();
-    
+
+    if (isset($_SESSION['pay'])){
+      unset($_SESSION['pay']);
+      header('Location: checkout.php');
+    }else{
+      $_SESSION['pay'] = "yes";
+      header('Location: checkout.php');
+    }
+
+    //make sure to unset after i use it check to see if set if set then undo
     ?>
   </body>
 </html>
