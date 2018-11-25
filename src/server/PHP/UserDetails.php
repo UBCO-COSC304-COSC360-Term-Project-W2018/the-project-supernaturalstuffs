ea49fdba8f928dff3cc90f3ecc3e22688a17204d
unknown <courtneygosselin1996@gmail.com>
2018-11-24 20:48:17 -0800

    td to th
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

--- src/server/PHP/UserDetails.php
+++ src/server/PHP/UserDetails.php
@@ -38,7 +38,7 @@
 						echo '<p>User Details</p>';
 						//List users details
 						echo '<table>';
-								echo '<tr><th>UserId</th><td>Username</th><th>First Name</th><th>Last Name</th><th>Email</th></th>';
+								echo '<tr><th>UserId</th><th>Username</th><th>First Name</th><th>Last Name</th><th>Email</th></th>';
 						foreach ($rows as $row) {
 							echo	'<tr><td>' . $row['userID'] . '</td><td>' . $row['username'] . '</td><td>' . $row['firstName'] . '</td><td>' . $row['lastName'] . '</td><td>' . $row['email'] . '</td></tr>';
 						}
