fe481f45665bcfeb42ec025fe863e5915738034f
unknown <courtneygosselin1996@gmail.com>
2018-11-25 14:20:34 -0800

    Add file for form add
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

---
+++ src/server/PHP/userForm.php
@@ -0,0 +1,65 @@
+<!--add user-->
+<!DOCTYPE html>
+<html>  
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+	<link href='https://fonts.googleapis.com/css?family=Almendra Display' rel='stylesheet'>
+    <link rel="stylesheet" href="../css/reset.css" />
+    <link rel="stylesheet" href="../css/header-footer.css" />
+    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+	<link rel="stylesheet" type="text/css" href="../css/admin.css">
+	<link rel="stylesheet" type="text/css" href="../css/UserDetails.css">
+		<!--<script src="script.js"></script>-->
+  </head>
+
+  <body>
+	<!--Include header-->
+	<?php include '../../../src/server/include/header.php'; ?>
+	<main>
+		<?php 
+			include '../include/db_credentials.php'; 
+			
+
+			//connect to database
+			try {
+				$pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (\PDOException $e) {
+				throw new \PDOException($e->getMessage(), (int)$e->getCode());
+			}
+			
+			$sql = 'SELECT * FROM Orders WHERE orderID= ?';
+			$statement = $pdo->prepare($sql);
+			$statement->execute(array($_GET["filter"]));
+			$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+			
+			echo('<div id="box">
+					<div id="Users">
+						<form action="addUser.php" method="get">
+							<h2>Add User</h2>
+							<div class="catagories">
+							<!--User id autoincrements"-->
+							<p>User\'s username:</p>
+							<input type="text" name="username" placeholder="User id>
+							<p>User\'s password:</p>
+							<input type="text" name="password" placeholder="User id>
+							<p>User\'s first name:</p>
+							<input type="text" name="firstname" placeholder="User id>				
+							<p>User\'s last name:</p>
+							<input type="text" name="lastname" placeholder="User id>
+							<p>User\'s email:</p>
+							<input type="text" name="email" plaeholder="User id>
+							
+							</div>
+						</form>
+					</div>
+				</div>;');
+				
+		?>
+		
+	</main>
+	<!--Footer include-->
+	<?php include '../../../src/server/include/footer.php'; ?>
+
+
+ </body>
