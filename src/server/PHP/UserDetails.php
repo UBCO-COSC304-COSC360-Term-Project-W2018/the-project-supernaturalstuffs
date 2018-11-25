b6781ad6acdc4c11aff4dbc0da9ca53f8f02ded4
unknown <courtneygosselin1996@gmail.com>
2018-11-24 19:46:20 -0800

    Add users reviews
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

--- src/server/PHP/UserDetails.php
+++ src/server/PHP/UserDetails.php
@@ -32,7 +32,7 @@
 			
 			echo('<div id="box">
 					<div id="Users">
-						<h2>Users</h2>
+						<h2>User</h2>
 						<div class="catagories">');
 						echo '<p>User Details</p>';
 						//List users details
@@ -59,6 +59,19 @@
 						echo '</table>';
 						
 						//User's Reviews
+						$sql = 'SELECT * FROM Reviews WHERE userID=?';
+						$statement = $pdo->prepare($sql);
+						$statement->execute(array($_GET['filter']));
+						$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+						echo "<p>User's Reviews</p>";
+						
+						echo '<table>';
+							echo '<tr><td>OrderID</td><td>Total Price</td><td>Tracking number</td><td>UserID</td><td>StoreID</td></tr>';
+						foreach ($rows as $row) {
+							echo	'<tr><td>' . $row['orderID'] . '</td><td>' . $row['totalPrice'] . '</td><td>' . $row['trackingNumber'] . '</td><td>' . $row['userID'] . '</td><td>' . $row['storeID'] . '</td></tr>';
+						}
+						echo '<tr rowspan="4"><td>Total Price: BLANK</td></tr>';
+						echo '</table>';
 								
 			echo(		'</div>
 					</div>
