42a8a45c8d4e8b17c693ec11e3ae61c56be53bda
unknown <courtneygosselin1996@gmail.com>
2018-11-26 21:35:26 -0800

    testing prepared statements
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

--- src/server/PHP/productsInformation.php
+++ src/server/PHP/productsInformation.php
@@ -35,7 +35,7 @@
 				$sql = 'SELECT * FROM Product WHERE pName LIKE pName = "?" OR category LIKE category = "?"';
 			}
 			
-			$getInfo = "%' .$_GET["filter"] . '%";
+			$getInfo = '%' . $_GET["filter"] . '%';
 			$statement = $pdo->prepare($sql);
 			$statement->execute(array($getInfo, $getInfo));
 			$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
