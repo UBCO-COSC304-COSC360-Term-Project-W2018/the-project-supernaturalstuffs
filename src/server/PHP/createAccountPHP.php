c6a58d403acb8d4d9eb49aa4f917e8627304c19e
Robert Rosa <robby.rosa@hotmail.com>
2018-11-26 19:46:51 -0800

    dont add photo to uploads test

--- src/server/PHP/createAccountPHP.php
+++ src/server/PHP/createAccountPHP.php
@@ -97,13 +97,13 @@
       if ($uploadOk == 0) {
           echo "Sorry, your file was not uploaded.";
       // if everything is ok, try to upload file
-      } else {
+    } /*else {
           if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
               echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
           } else {
               echo "Sorry, there was an error uploading your file.";
           }
-      }
+      }*/
 
       //connect to database
       try {
