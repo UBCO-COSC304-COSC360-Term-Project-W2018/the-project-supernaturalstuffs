57496a47ef845732a104d4c739195aa2bbd00be2
Robert Rosa <robby.rosa@hotmail.com>
2018-11-27 21:14:03 -0800

    okay

--- src/server/PHP/addcart.php
+++ src/server/PHP/addcart.php
@@ -1,30 +1,30 @@
 <?php
-// Get the current list of products
-session_start();
-$productList = null;
-if (isset($_SESSION['productList'])){
-	$productList = $_SESSION['productList'];
-} else{ 	// No products currently in list.  Create a list.
-	$productList = array();
-}
+		// Get the current list of products
+		session_start();
+		$productList = null;
+		if (isset($_SESSION['productList'])){
+			$productList = $_SESSION['productList'];
+		} else{ 	// No products currently in list.  Create a list.
+			$productList = array();
+		}
 
-// Add new product selected
-// Get product information
-if(isset($_GET['id']) && isset($_GET['pName']) && isset($_GET['price'])){
-	$id = $_GET['id'];
-	$name = $_GET['pName'];
-	$price = $_GET['price'];
-} else {
-	header('Location: listprod.php');
-}
+		// Add new product selected
+		// Get product information
+		if(isset($_GET['pID']) && isset($_GET['pName']) && isset($_GET['price'])){
+			$pID = $_GET['pID'];
+			$pName = $_GET['pName'];
+			$price = $_GET['price'];
+		} else {
+			header('Location: products.php');
+		}
 
-// Update quantity if add same item to order again
-if (isset($productList[$id])){
-	$productList[$id]['quantity'] = $productList[$id]['quantity'] + 1;
-} else {
-	$productList[$id] = array( "id"=>$id, "pName"=>$name, "price"=>$price, "quantity"=>1 );
-}
+		// Update quantity if add same item to order again
+		if (isset($productList[$id])){
+			$productList[$pID]['quantity'] = $productList[$pID]['quantity'] + 1;
+		} else {
+			$productList[$pID] = array( "pID"=>$pID, "pName"=>$name, "price"=>$price, "quantity"=>1 );
+		}
 
-$_SESSION['productList'] = $productList;
-header('Location: cart.php');
+		$_SESSION['productList'] = $productList;
+		header('Location: products.php');
 ?>
