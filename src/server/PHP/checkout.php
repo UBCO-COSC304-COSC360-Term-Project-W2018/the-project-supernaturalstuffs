9809710a3a42154fb31f50fd6d1f63360c1282d0
Robert Rosa <robby.rosa@hotmail.com>
2018-11-28 19:45:13 -0800

    cart not row

--- src/server/PHP/checkout.php
+++ src/server/PHP/checkout.php
@@ -205,11 +205,11 @@
             <?php
             if (isset($_SESSION['productList'])){
               foreach ($cart as $pID => $cartitem){
-                $image = $row['image'];
+                $image = $cart['image'];
 								$type = "png";
 
                 echo "<div class='items'>";
-                  echo	'<a href="individualProducts.php?pID='.$row['pID'].'"><img src = "data:image/'.$type.';base64, '.base64_encode($image).'"/></a>';
+                  echo	'<a href="individualProducts.php?pID='.$cart['pID'].'"><img src = "data:image/'.$type.';base64, '.base64_encode($image).'"/></a>';
                   echo "<div class='productinfo'>";
                     echo "<p>Product name: ".$cartitem['pName']."</p>";
                     echo "<p>Price: ".str_replace("USD","$",money_format('%i',$cartitem['price']))."</p>";
