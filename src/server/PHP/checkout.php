3d30c2be53d4d179fe706e350f2af0293d753d7f
Robert Rosa <robby.rosa@hotmail.com>
2018-11-28 18:08:23 -0800

    courtney help

--- src/server/PHP/checkout.php
+++ src/server/PHP/checkout.php
@@ -243,7 +243,7 @@
                     echo "<p>Product name: ".$cartitem['pName']."</p>";
                     echo "<p>Price: ".str_replace("USD","$",money_format('%i',$cartitem['price']))."</p>";
                     echo "<p>Quantity: ".$cartitem['quantity']."</p>";
-                    echo ("<input class ='button' type='button' name='delete' value='Delete' onclick='href=\'?id='".$cartitem['pID']."'\'/>");
+                    echo ("<input class ='button' type='button' name='delete' value='Delete' onclick='href=?id='".$cartitem['pID']."'/>");
                   echo "</div>";
                 echo "</div>";
               }
