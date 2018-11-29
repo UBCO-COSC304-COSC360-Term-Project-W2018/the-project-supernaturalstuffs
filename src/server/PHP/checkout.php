482696d6f3a488402b222f59a3172eb0d2cae17e
Robert Rosa <robby.rosa@hotmail.com>
2018-11-28 18:41:14 -0800

    might have got it!!

--- src/server/PHP/checkout.php
+++ src/server/PHP/checkout.php
@@ -211,7 +211,7 @@
                     echo "<p>Product name: ".$cartitem['pName']."</p>";
                     echo "<p>Price: ".str_replace("USD","$",money_format('%i',$cartitem['price']))."</p>";
                     echo "<p>Quantity: ".$cartitem['quantity']."</p>";
-                    echo "<input class ='button' type='button' name='delete' value='delete' onclick='href=?pID=".$cartitem['pID']."'/>";
+                    echo "<input class ='button' type='button' name='delete' value='delete' onclick='href=\"?pID='.$cartitem['pID'].'\"/>";
                     // onclick='href=?id=".$cartitem['pID']."' made me mad
                   echo "</div>";
                 echo "</div>";
