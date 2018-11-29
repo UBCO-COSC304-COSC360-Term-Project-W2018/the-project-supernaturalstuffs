e0eeb09dc22c782b17751adc96823ae67d432201
Robert Rosa <robby.rosa@hotmail.com>
2018-11-28 16:36:17 -0800

    style

--- src/server/PHP/order.php
+++ src/server/PHP/order.php
@@ -195,7 +195,7 @@
 
         $total = 0;
         foreach ($cart as $id => $prod) {
-        		echo("<tr><td>". $prod['pID'] . "</td>");
+        		echo("<tr class='product'><td>". $prod['pID'] . "</td>");
         		echo("<td>" . $prod['pName'] . "</td>");
 
         		echo("<td align=\"center\">". $prod['quantity'] . "</td>");
@@ -206,10 +206,10 @@
         		echo("</tr>");
         		$total = $total +$prod['quantity']*$price;
         }
-        echo("<tr><td colspan=\"4\" align=\"right\"><b>Product Total</b></td><td align=\"right\">".str_replace("USD","$",money_format('%i',$total))."</td></tr>");
+        echo("<tr><td colspan=\"4\" align=\"right\"><b>Product Total:</b></td><td align=\"right\">".str_replace("USD","$",money_format('%i',$total))."</td></tr>");
         $total = $total + $shipDelivery;
-        echo("<tr><td colspan=\"4\" align=\"right\"><b>Shipping Total</b></td><td align=\"right\">".str_replace("USD","$",money_format('%i',$shipDelivery))."</td></tr>");
-        echo("<tr><td colspan=\"4\" align=\"right\"><b>Order Total</b></td><td align=\"right\">".str_replace("USD","$",money_format('%i',$total))."</td></tr>");
+        echo("<tr><td colspan=\"4\" align=\"right\"><b>Shipping Total:</b></td><td align=\"right\">".str_replace("USD","$",money_format('%i',$shipDelivery))."</td></tr>");
+        echo("<tr><td colspan=\"4\" align=\"right\"><b>Order Total:</b></td><td align=\"right\">".str_replace("USD","$",money_format('%i',$total))."</td></tr>");
         echo("</table>");
         echo("<h1>Order completed. Will be shipped soon...</h1>");
         echo("<h1>Your order reference number is: " . $orderID . '</h1>');
