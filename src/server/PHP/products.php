512035055cfb3412fff0b47a19aba65bc5795027
Robert Rosa <robby.rosa@hotmail.com>
2018-11-29 15:42:08 -0800

    products?

--- src/server/PHP/products.php
+++ src/server/PHP/products.php
@@ -32,34 +32,84 @@
 							<div id="box">');
 
 							//check if All or another selection
-							if(!(isset($_GET["filter"]))){
+							if(!(isset($_GET["filter"])) && !(isset($_GET["top"]))){
 								$sql = 'SELECT * FROM Product';
 
-							}else{
+                $statement = $pdo->prepare($sql);
+  							$statement->execute();
+  							$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+  							foreach ($rows as $row) {
+  								$image = $row['image'];
+  								$type = "png";
+
+  								echo '<div class="productBox">';
+  								echo	'<a href="individualProducts.php?pID='.$row['pID'].'"><img src = "data:image/'.$type.';base64, '.base64_encode($image).'"/></a>';
+
+  								echo	'<p>' . $row["pName"] . '</p>';
+  								echo	'<p>' . $row["price"] . '</p>';
+
+  								echo	'<a href=\'addToCart.php?pID='.$row['pID'].'&pName='.$row['pName'].'&price='.$row['price']."&description=".$row['description'].'\'><p class="addCart">Add to Cart</p></a>';
+
+  								echo '</div>';
+  							}
+
+							}else if(isset($_GET["top"])){
+                $sql2 ='SELECT pID, COUNT(pID) FROM InOrder GROUP BY pID ORDER BY COUNT(pID) DESC LIMIT 5';
+
+                $statement = $pdo->prepare($sql2);
+  							$statement->execute();
+  							$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($rows as $row) {
+
+                  $sql = 'SELECT * FROM Product WHERE pID = :pID' ;
+
+                  $statement = $pdo->prepare($sql);
+                  $statement->bindParam(':pID',$row['pID'], PDO::PARAM_INT);
+    							$statement->execute();
+    							$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+    							foreach ($rows as $row) {
+    								$image = $row['image'];
+    								$type = "png";
+
+    								echo '<div class="productBox">';
+    								echo	'<a href="individualProducts.php?pID='.$row['pID'].'"><img src = "data:image/'.$type.';base64, '.base64_encode($image).'"/></a>';
+
+    								echo	'<p>' . $row["pName"] . '</p>';
+    								echo	'<p>' . $row["price"] . '</p>';
+
+    								echo	'<a href=\'addToCart.php?pID='.$row['pID'].'&pName='.$row['pName'].'&price='.$row['price']."&description=".$row['description'].'\'><p class="addCart">Add to Cart</p></a>';
+
+    								echo '</div>';
+                }
+
+              }else{
 								$sql = 'SELECT * FROM Product WHERE pName LIKE "%' . $_GET["filter"] . '%" OR category LIKE "%' . $_GET["filter"] . '";';
+
+                $statement = $pdo->prepare($sql);
+  							$statement->execute();
+  							$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+  							foreach ($rows as $row) {
+  								$image = $row['image'];
+  								$type = "png";
+
+  								echo '<div class="productBox">';
+  								echo	'<a href="individualProducts.php?pID='.$row['pID'].'"><img src = "data:image/'.$type.';base64, '.base64_encode($image).'"/></a>';
+
+  								echo	'<p>' . $row["pName"] . '</p>';
+  								echo	'<p>' . $row["price"] . '</p>';
+
+  								echo	'<a href=\'addToCart.php?pID='.$row['pID'].'&pName='.$row['pName'].'&price='.$row['price']."&description=".$row['description'].'\'><p class="addCart">Add to Cart</p></a>';
+
+  								echo '</div>';
+  							}
 							}
 
-							$statement = $pdo->prepare($sql);
-							$statement->execute();
-							$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-							foreach ($rows as $row) {
-								$image = $row['image'];
-								$type = "png";
-
-								echo '<div class="productBox">';
-								echo	'<a href="individualProducts.php?pID='.$row['pID'].'"><img src = "data:image/'.$type.';base64, '.base64_encode($image).'"/></a>';
-
-								echo	'<p>' . $row["pName"] . '</p>';
-								echo	'<p>' . $row["price"] . '</p>';
-
-								echo	'<a href=\'addToCart.php?pID='.$row['pID'].'&pName='.$row['pName'].'&price='.$row['price']."&description=".$row['description'].'\'><p class="addCart">Add to Cart</p></a>';
-
-								echo '</div>';
-
-							}
 						echo '</div>';
 					?>
 			</div>
