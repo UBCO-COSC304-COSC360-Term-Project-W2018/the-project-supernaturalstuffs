69da5cc6c76b2145b88939f13ee404426744c94a
Robert Rosa <robby.rosa@hotmail.com>
2018-11-28 23:26:47 -0800

    5.0

--- src/server/PHP/checkout.php
+++ src/server/PHP/checkout.php
@@ -221,11 +221,11 @@
                     echo "<p>Product name: ".$cartitem['pName']."</p>";
                     echo "<p>Price: ".str_replace("USD","$",money_format('%i',$cartitem['price']))."</p>";
                     echo "<p>Quantity: ".$cartitem['quantity']."</p>";
-                    echo "<form  name='updateForm' method='get' action='changeQuantity.php?pID=".$cartitem['pID']."' id='quantityForm'>";
+                    echo "<form  name='updateForm' method='get' action='updateQuantity.php?pID=".$cartitem['pID']."' id='quantityForm'>";
                       echo "<input type='number' name='quantity' id='quantityInput'/>";
                       echo "<input class ='button' type='submit' name='update' value='Update' id='update'/>";
+                      echo "<a href='?pID=".$cartitem['pID']."'><input class ='button' type='button' name='delete' value='Delete'/></a>";
                     echo "</form>";
-                    echo "<a href='?pID=".$cartitem['pID']."'><input class ='button' type='button' name='delete' value='Delete'/></a>";
                   echo "</div>";
                 echo "</div>";
               }
