7aa38990a7c691a650aedde44c18ed8ecb9f226a
lcamp30 <lcamp30@outlook.com>
2018-11-29 17:20:40 -0800

    fixxx

--- src/server/PHP/reviews.php
+++ src/server/PHP/reviews.php
@@ -4,6 +4,12 @@ if(isset($_SESSION['email'])) {
     $custE = $_SESSION['email'];
     $id = $_POST["pID"];
 
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
     $sql = "SELECT userID FROM User WHERE email = :email";
     $statement = $pdo->prepare($sql);
     $statement->bindParam(':email', $custE, PDO::PARAM_STR);
