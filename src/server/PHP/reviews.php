fb08bba7bcea2f1870f358a4209182b28e1d4bd0
lcamp30 <lcamp30@outlook.com>
2018-11-29 17:04:29 -0800

    Merge branch 'master' of https://github.com/UBCO-COSC304-COSC360-Term-Project-W2018/the-project-supernaturalstuffs

--- src/server/PHP/reviews.php
+++ src/server/PHP/reviews.php
@@ -1,5 +1,8 @@
 <?php
+session_start();
 if(isset($_SESSION['email'])) {
+    $id = $_GET["pID"];
+    $sql = "SELECT userID FROM User";
     $sql = "INSERT INTO Reviews(comment) VALUES ('" . $_POST['comment'] . "')";
 
     $result = mysql_query($sql);
@@ -8,7 +11,9 @@ if(isset($_SESSION['email'])) {
 } else {
     $message = "You must be signed in to write a review";
     echo "<script type='text/javascript'>alert('$message');
-    window.location.href='/products.php'</script>";
+    window.location.href='/individualProducts.php?pID='.$id.'</script>";
 }
 
 ?>
+
+//reviews table - userID, pID, comment
