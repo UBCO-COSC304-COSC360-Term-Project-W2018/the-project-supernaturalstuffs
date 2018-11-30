e7a9b59d16aab70e8680ce13caa8f947ef37584e
lcamp30 <lcamp30@outlook.com>
2018-11-29 17:58:39 -0800

    Merge branch 'master' of https://github.com/UBCO-COSC304-COSC360-Term-Project-W2018/the-project-supernaturalstuffs

--- src/server/PHP/individualProducts.php
+++ src/server/PHP/individualProducts.php
@@ -63,8 +63,7 @@
               }
               echo '<form class="review" method="post" action="reviews.php?id='.$pID.'>';
               echo '<div>';
-              echo '<label>Write a review: </label>';
-
+              echo '<p>Write a review: </p>';
               echo '<input type="text" id="textbox" name="comment"/>';
               echo '</div>';
               echo '<input type ="submit" value="Submit" class="addCart">';
