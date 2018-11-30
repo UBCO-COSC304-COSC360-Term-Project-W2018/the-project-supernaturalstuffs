e20c31c192e0d7bf6091fc68b5ad9b1cea3d9d7e
unknown <courtneygosselin1996@gmail.com>
2018-11-29 18:11:44 -0800

    CSS cart time
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

--- src/server/include/header.php
+++ src/server/include/header.php
@@ -4,7 +4,7 @@
         <form action="/src/server/PHP/products.php" method="get">
             <input type="text" name="filter" class="searchHome" placeholder="Search...">
             <img src="/src/client/images/search.png" alt="search" id="search">
-			<a href="/src/server/PHP/cart.php"><img src="/src/client/images/cart.png" alt="shopping cart" id="cart"></a>
+			<a href="/src/server/PHP/cart.php"><img src="/src/client/images/cart.png" alt="shopping cart" id="cart"><p id="inCart">0</p></a>
 	   </form>
     </div>
     <nav>
