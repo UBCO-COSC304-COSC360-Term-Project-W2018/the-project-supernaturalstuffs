1e1f557f8d2361be629d2771974ca5a838ce07f2
Robert Rosa <robby.rosa@hotmail.com>
2018-11-30 13:53:25 -0800

    row not rows

--- src/server/PHP/delete.php
+++ src/server/PHP/delete.php
@@ -100,11 +100,11 @@
             $numRows = $numRows + "1";
           }
           if($numRows > "0"){
-            $quantity = $rows2['quantity'] + $row['quantity'];
+            $quantity = $row2['quantity'] + $row['quantity'];
           }else{
             $quantity = $row['quantity'];
           }
-          
+
           $sql2 = "UPDATE Stock SET quantity=:quantity WHERE pID = :pID AND storeID = :storeID";
           $statement = $pdo->prepare($sql2);
           $statement->bindValue(':quantity', $quantity , PDO::PARAM_INT);
