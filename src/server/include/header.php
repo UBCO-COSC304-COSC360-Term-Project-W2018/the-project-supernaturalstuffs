a2629cbe27004b16553f9be9b2b5b3de17a218fe
unknown <courtneygosselin1996@gmail.com>
2018-11-29 18:21:11 -0800

    Hell on earth
    Signed-off-by: unknown <courtneygosselin1996@gmail.com>

--- src/server/include/header.php
+++ src/server/include/header.php
@@ -4,7 +4,7 @@
         <form action="/src/server/PHP/products.php" method="get">
             <input type="text" name="filter" class="searchHome" placeholder="Search...">
             <img src="/src/client/images/search.png" alt="search" id="search">
-			<a href="/src/server/PHP/cart.php"><img src="/src/client/images/cart.png" alt="shopping cart" id="cart"><p id="inCart">0</p></a>
+			<a href="/src/server/PHP/cart.php"><img src="/src/client/images/cart.png" alt="shopping cart" id="cart"><span id="inCart">0</span></a>
 	   </form>
     </div>
     <nav>
